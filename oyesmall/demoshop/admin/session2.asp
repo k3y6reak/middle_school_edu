f9990886578998df7695ad84c219a6875915657a
k3y6reak <k3y6reak@k3y6reak.local>
2017-08-16 15:56:50 +0900

    middle_school_edu

--- oyesmall/demoshop/admin/session2.asp
+++
@@ -1,9 +0,0 @@
-<%
-	IF	Request.Cookies("MemLog")("cmemlogon") <> "yes" then
-			Response.Redirect "./admin_login.asp"
-	Else
-	
-			admingubun = Request.Cookies("MemLog")("admingubun")
-
-	End IF
-%>
--- oyesmall/demoshop/shop_file/func.js
+++
@@ -1,4 +0,0 @@
-	function fnPopupEvent(vcid, vid, vname, vprice, vkano) {
-		var strURL ="http://211.218.144.142/event_step1.asp?catalog_id="+vcid+"&user_id="+vid+"&user_name="+vname+"&price="+vprice+"&wizaddr="+vkano;
-		window.open(strURL, 'EventWin3', 'width=440,height=479,srollbars=auto');
-    }
